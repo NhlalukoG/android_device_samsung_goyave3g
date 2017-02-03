8aee9e7daa620c3f2fbd5b38f47c0cb004462d99
NhlalukoG <ngjmasingi@gmail.com>
2017-02-04 01:02:07 +0200

    First commit cm13 device tree

---
+++ BoardConfig.mk
@@ -0,0 +1,146 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from SPRD common configs
+-include device/samsung/sprd-common/BoardConfigCommon.mk
+
+# Inherit from the proprietary version
+-include vendor/samsung/goyave3g/BoardConfigVendor.mk
+
+# Platform
+TARGET_ARCH := arm
+TARGET_BOARD_PLATFORM := sc8830
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_BOOTLOADER_BOARD_NAME := sc7730s
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a7 -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a7 -mfpu=neon -mfloat-abi=softfp
+BOARD_VENDOR := samsung
+
+# Config u-boot
+TARGET_NO_BOOTLOADER := true
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 5901385728
+BOARD_CACHEIMAGE_PARTITION_SIZE := 209715200
+BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+
+# Kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
+BOARD_KERNEL_PAGESIZE := 2048
+TARGET_KERNEL_CONFIG := goyave3g_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/goyave3g
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/goyave3g/dt.img
+
+# RIL
+BOARD_RIL_CLASS += ../../../device/samsung/goyave3g/ril
+COMMON_GLOBAL_CFLAGS += -DDISABLE_ASHMEM_TRACKING
+
+# FM radio
+BOARD_HAVE_FM_BCM := true
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/goyave3g/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/goyave3g/bluetooth/libbt_vndcfg.txt
+
+# Wifi
+BOARD_WLAN_DEVICE := bcmdhd
+BOARD_WLAN_DEVICE_REV := bcm4343
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA := "/system/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_P2P := "/system/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_NVRAM_PATH_PARAM := "/sys/module/dhd/parameters/nvram_path"
+WIFI_DRIVER_NVRAM_PATH := "/system/etc/wifi/nvram_net.txt"
+WIFI_BAND := 802_11_ABG
+BOARD_HAVE_SAMSUNG_WIFI := true
+
+# Graphics
+HWUI_COMPILE_FOR_PERF := true
+TARGET_REQUIRES_SYNCHRONOUS_SETSURFACE := true
+
+# HWComposer
+USE_SPRD_HWCOMPOSER := true
+USE_SPRD_DITHER := true
+USE_OVERLAY_COMPOSER_GPU := true
+TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
+
+# Lights
+TARGET_HAS_BACKLIT_KEYS := false
+
+# Resolution
+TARGET_SCREEN_HEIGHT := 1026
+TARGET_SCREEN_WIDTH := 600
+
+# Audio
+BOARD_USE_LIBATCHANNEL_WRAPPER := true
+USE_LEGACY_AUDIO_POLICY := 1
+
+# Media
+COMMON_GLOBAL_CFLAGS += -DBOARD_CANT_REALLOCATE_OMX_BUFFERS
+
+# Board specific features
+BOARD_USE_SAMSUNG_COLORFORMAT := true
+COMMON_GLOBAL_CFLAGS += -DSPRD_HARDWARE
+
+# healthd
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.sc8830
+
+# Init
+TARGET_NR_SVC_SUPP_GIDS := 36
+TARGET_PROVIDES_INIT_RC := true
+TARGET_NEEDS_PROP_INIT_HACK := true
+
+# Recovery
+BOARD_HAS_DOWNLOAD_MODE := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+BOARD_SUPPRESS_EMMC_WIPE := true
+TARGET_RECOVERY_FSTAB := device/samsung/goyave3g/rootdir/recovery.fstab
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := SM-T116,SM-T116U,goyave,goyave3g,goyawifi
+
+# SELinux
+BOARD_SEPOLICY_DIRS += device/samsung/goyave3g/sepolicy
+
+# Enable dex-preoptimization to speed up the first boot sequence
+WITH_DEXPREOPT := true
+WITH_DEXPREOPT_PIC := true
+WITH_DEXPREOPT_COMP := false
+
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_NO_CHARGER_LED := true
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+CHARGING_ENABLED_PATH := /sys/class/power_supply/battery/batt_lp_charging
+BACKLIGHT_PATH := /sys/class/backlight/panel/brightness
+
---
+++ cm.mk
@@ -0,0 +1,19 @@
+## Specify phone tech before including full_phone	
+$(call inherit-product, vendor/cm/config/telephony.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := goyave3g
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, $(LOCAL_PATH)/device_goyave3g.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := goyave3g
+PRODUCT_NAME := cm_goyave3g
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-T116
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := phone
